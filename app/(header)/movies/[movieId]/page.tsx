30f9ecf0c1d6d9eb6b2df0192c282da7182058ba
Hyukmin Kim <hyukminkim@Hyukminui-MacBookPro.local>
2026-07-04 16:31:36 +0900

    메타데이터 동적 데이터로 수정

--- app/(header)/movies/[movieId]/page.tsx
+++ app/(header)/movies/[movieId]/page.tsx
@@ -44,23 +44,55 @@ export interface Rating {
   Source: string
   Value: string
 }
-export const metadata = {
-  title: '영화 상세 정보',
-  description: '영화 상세 정보 설명입니다',
-  openGraph: {
-    type: 'website',
-    siteName: 'next.js 연습 project',
-    title: '영화 상세 정보',
-    description: '영화 상세 정보 설명입니다',
-    images: 'https://picsum.photos/700/500'
+// export const metadata = {
+//   title: '영화 상세 정보',
+//   description: '영화 상세 정보 설명입니다',
+//   openGraph: {
+//     type: 'website',
+//     siteName: 'next.js 연습 project',
+//     title: '영화 상세 정보',
+//     description: '영화 상세 정보 설명입니다',
+//     images: 'https://picsum.photos/700/500'
+//   }
+// }
+
+async function fetchMovie(movieId: string) {
+  // const { data: movie } = await axios.get<Movie>(
+  //   `https://omdbapi.com?apikey=${process.env.OMDB_APIKEY}&i=${movieId}`
+  // )
+  // 여기서의 fetch함수는 오리지널이 아닌 next.js용 fetch 함수이다
+  const res = await fetch(
+    `https://omdbapi.com?apikey=${process.env.OMDB_APIKEY}&i=${movieId}`,
+    {
+      //mehtod: 'GET'
+      cache: 'force-cache' // next.js 전용 기능(서버에서 캐시됨)
+    }
+  )
+  const movie = await res.json()
+  return movie
+}
+
+// 'generateMetadata' 이름 변경 불가
+export async function generateMetadata({ params }: Props) {
+  const { movieId } = await params
+  const movie = await fetchMovie(movieId)
+  return {
+    title: movie.Title,
+    description: movie.Plot,
+    openGraph: {
+      type: 'website',
+      siteName: 'next.js 연습 project',
+      title: movie.Title,
+      description: movie.Plot,
+      images: movie.Poster
+    }
   }
 }
+
 // 애초에 params가 들어온다, promise 객체이기 때문에 이행 해야만 값을 얻을 수 있다
 export default async function MovieDetails({ params }: Props) {
   const { movieId } = await params
-  const { data: movie } = await axios.get<Movie>(
-    `https://omdbapi.com?apikey=${process.env.OMDB_APIKEY}&i=${movieId}`
-  )
+  const movie = await fetchMovie(movieId)
   return (
     <>
       <h1>{movie.Title}</h1>
